1d51bba13805457eef2948df4e2bbe132ed83af6
iambasantarai <iambasantarai@gmail.com>
2024-03-08 07:52:57 +0545

    Clear the uploads directory when a new upload request is made

--- app.py
+++ app.py
@@ -25,44 +25,6 @@ def allowed_file(filename):
     return "." in filename and filename.split(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def stitch_images(images):
-    stitcher = Stitcher()
-
-    no_of_images = len(images)
-
-    # Modify the images width and height to keep the aspect ratio the same across images
-    for i in range(no_of_images):
-        images[i] = imutils.resize(images[i], width=800)
-
-    for i in range(no_of_images):
-        images[i] = imutils.resize(images[i], height=800)
-
-    if no_of_images == 2:
-        (result, matched_points) = stitcher.image_stitch(
-            [images[0], images[1]], match_status=True
-        )
-    else:
-        (result, matched_points) = stitcher.image_stitch(
-            [images[no_of_images - 2], images[no_of_images - 1]], match_status=True
-        )
-        for i in range(no_of_images - 2):
-            (result, matched_points) = stitcher.image_stitch(
-                [images[no_of_images - i - 3], result], match_status=True
-            )
-
-    # Save stitcher and matched_points images in the 'static/output' folder
-    output_folder = "static/output"
-    os.makedirs(output_folder, exist_ok=True)
-
-    panorama_path = os.path.join(output_folder, "panorama_image.jpg")
-    matched_points_path = os.path.join(output_folder, "matched_points.jpg")
-
-    cv2.imwrite(panorama_path, result)
-    cv2.imwrite(matched_points_path, matched_points)
-
-    return panorama_path, matched_points_path
-
-
 @app.route("/heartbeat", methods=["GET"])
 def heartbeat():
     heartbeat = time.monotonic_ns()
@@ -72,6 +34,11 @@ def heartbeat():
 @app.route("/upload", methods=["POST"])
 def upload_image():
     try:
+
+        # Clear the contents of the 'uploads' folder before saving new files
+        shutil.rmtree(UPLOAD_FOLDER)
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
         # check if the post request contains files
         if "files[]" not in request.files:
             return jsonify({"message": "Images are required!"}), 400
@@ -102,40 +69,6 @@ def upload_image():
 
     except Exception as e:
         return jsonify({"message": e}), 500
-
-
-@app.route("/stitch-opencv", methods=["GET"])
-def stitch_api():
-    try:
-        # Get the list of uploaded files from the 'uploads' directory
-        uploaded_files = os.listdir(UPLOAD_FOLDER)
-
-        if len(uploaded_files) < 2:
-            return (
-                jsonify({"message": "At least two images are required for stitching!"}),
-                400,
-            )
-
-        filename = [
-            os.path.join(UPLOAD_FOLDER, file_name) for file_name in uploaded_files
-        ]
-        images = [cv2.imread(file) for file in filename]
-
-        panorama_path, matched_points_path = stitch_images(images)
-
-        return (
-            jsonify(
-                {
-                    "message": "Images stitched successfully!",
-                    "panorama_image_path": panorama_path,
-                    "matched_points_path": matched_points_path,
-                }
-            ),
-            200,
-        )
-
-    except Exception as e:
-        return jsonify({"message": str(e)}), 500
 
 
 @app.route("/generate-panorama", methods=["GET"])
@@ -208,6 +141,7 @@ def delete_uploads():
 def serve_files(filename):
     return send_from_directory(os.path.join(UPLOAD_FOLDER, RESULTS_FOLDER), filename)
 
+
 @app.route("/serve-all-files")
 def serve_all_files():
     try:
@@ -220,6 +154,7 @@ def serve_all_files():
 
     except Exception as e:
         return jsonify({"message": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
